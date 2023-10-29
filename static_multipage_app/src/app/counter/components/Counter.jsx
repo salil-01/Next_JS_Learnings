15014fa81cda454205da455ab3357c0b97ee40a0
Salil Upadhyay <upsalil.01@gmail.com>
2023-10-30 00:10:46 +0530

    next

---
+++ static_multipage_app/src/app/counter/components/Counter.jsx
@@ -0,0 +1,3 @@
+export default CounterLayout = () => {
+  return <>Hii from Counter Layout</>;
+};
---
+++ static_multipage_app/src/app/counter/page.js
@@ -0,0 +1,8 @@
+import { Counter } from "@/components/Counter";
+export default function CounterPage() {
+  return (
+    <>
+      <Counter />
+    </>
+  );
+}
